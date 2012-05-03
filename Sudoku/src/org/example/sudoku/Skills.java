2d32c4f13fb867fe180f65585bc06bd5f5026063
tunglxx226 <tunglxx226@gmail.com>
2012-05-03 14:28:51 +0700

    New video

---
+++ Sudoku/src/org/example/sudoku/Skills.java
@@ -0,0 +1,23 @@
+package org.example.sudoku;
+
+public class Skills 
+{
+	
+	private String name;
+	
+	public Skills()
+	{
+		
+	}
+	
+	public Skills(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+}
