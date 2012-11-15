5e3fa5a575cda3c8d5d227af27d640db282abc2a
tunglxx226 <tunglxx226@gmail.com>
2012-11-15 12:06:15 +0700

    Fixed quiz generating mechanism.

--- Sudoku/src/org/example/sudoku/Quiz.java
+++ Sudoku/src/org/example/sudoku/Quiz.java
@@ -3,18 +3,25 @@ package org.example.sudoku;
 import java.io.InputStream;
 import java.util.Random;
 
+import android.util.Log;
+
 public class Quiz {
 	public static final int DIFFICULTY_EASY = 0;
 	public static final int DIFFICULTY_MEDIUM = 1;
 	public static final int DIFFICULTY_HARD = 2;
 
-	public static final int MISSING_NUMBER_EASY = 30;
-	public static final int MISSING_NUMBER_MEDIUM = 45;
-	public static final int MISSING_NUMBER_HARD = 60;
+	// public static final int MISSING_NUMBER_EASY = 50;
+	// public static final int MISSING_NUMBER_MEDIUM = 60;
+	// public static final int MISSING_NUMBER_HARD = 70;
+
+	public static final int LEAP_EASY = 5;
+	public static final int LEAP_MEDIUM = 4;
+	public static final int LEAP_HARD = 3;
 
 	private String defResult;
 	private String quiz;
-	private int missingNumber;
+	//private int missingNumber;
+	private int leap;
 
 	InputStream is;
 
@@ -27,48 +34,68 @@ public class Quiz {
 	public void preparation(int difficulty) {
 		switch (difficulty) {
 		case DIFFICULTY_EASY:
-			missingNumber = MISSING_NUMBER_EASY;
+			// missingNumber = MISSING_NUMBER_EASY;
+			leap = LEAP_EASY;
 			break;
 		case DIFFICULTY_MEDIUM:
-			missingNumber = MISSING_NUMBER_MEDIUM;
+			// missingNumber = MISSING_NUMBER_MEDIUM;
+			leap = LEAP_MEDIUM;
 			break;
 		case DIFFICULTY_HARD:
-			missingNumber = MISSING_NUMBER_HARD;
+			// missingNumber = MISSING_NUMBER_HARD;
+			leap = LEAP_HARD;
 			break;
 		default:
 			break;
 		}
-		generate(missingNumber);
+		generate(leap);
 
 	}
 
-	public void generate(int missingNumber) {
-		int randomPosition[] = new int[missingNumber];
+	public void generate(int leap) {
+		// int randomPosition[] = new int[missingNumber];
+		int randomPosition[] = new int[81];
+		int missingNo = 0;
+		int i = 1; // index
+		int step = 0;
+
 		Random rand = new Random();
 		StringBuilder stringBuilder = new StringBuilder(quiz);
 
-		for (int i = 0; i < missingNumber; i++) {
-			//do {
-				randomPosition[i] = rand.nextInt(81);
-			//} while (checkDuplicate(randomPosition, i));
+		randomPosition[0] = rand.nextInt(leap);
+
+		missingNo++;
+
+		while (randomPosition[i - 1] < 81) {
+			step = rand.nextInt(leap);
+			if (step == 0) {
+				step = 1;
+			}
+
+			randomPosition[i] = randomPosition[i - 1] + step;
+			if (randomPosition[i] >= 81) {
+				randomPosition[i] = 80;
+				break;
+			}
+			i++;
+			missingNo++;
 		}
-		for (int i = 0; i < missingNumber; i++) {
-			stringBuilder.setCharAt(randomPosition[i], '0');
+
+		// }
+		for (int j = 1; j < missingNo; j++) {
+			stringBuilder.setCharAt(randomPosition[j], '0');
 		}
 		quiz = stringBuilder.toString();
 	}
 
-	
 	/*
-	 * This causes the system to be freezed due to too many loop
-	public boolean checkDuplicate(int randomPostion[], int position) {
-		for (int i = 0; i < position; i++) {
-			if (randomPostion[position] == randomPostion[i])
-				return false;
-
-		}
-		return true;
-	}*/
+	 * This causes the system to be freezed due to too many loop public boolean
+	 * checkDuplicate(int randomPostion[], int position) { for (int i = 0; i <
+	 * position; i++) { if (randomPostion[position] == randomPostion[i]) return
+	 * false;
+	 * 
+	 * } return true; }
+	 */
 
 	public String getQuiz() {
 		return quiz;
